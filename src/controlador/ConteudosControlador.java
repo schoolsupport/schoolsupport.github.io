7ac2a6023288a0e213eb3dcccaa4a244971f2143
viniciuscrizel <vinicius.crizel845@gmail.com>
2015-11-11 11:18:48 -0200

    pagina de mostrar onteudo pronta

--- src/controlador/ConteudosControlador.java
+++ src/controlador/ConteudosControlador.java
@@ -1,18 +1,24 @@
 package controlador;
 
-import spark.*;
+import java.util.HashMap;
+
+import modelo.Materia;
+import persistencia.MateriaDAO;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.TemplateViewRoute;
 
 public class ConteudosControlador implements TemplateViewRoute{
 
 	@Override
 	public ModelAndView handle(Request req, Response res) {
-		
-		
-		
-		
-		
-		
-		return null;
+		Materia materia;
+		MateriaDAO dao = new MateriaDAO();
+		materia = dao.busca(10);		
+		HashMap dados = new HashMap();
+		dados.put("conteudo", materia);
+		req.session().attribute("conteudo", materia);
+		return new ModelAndView(dados, "conteudos.html");
 	}
-	
 }
--- src/modelo/Materia.java
+++ src/modelo/Materia.java
@@ -5,7 +5,18 @@ public class Materia {
 	private String disciplina;
 	private String titulo;
 	private String conteudo;
+	private int code;
 	
+	
+	
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
 	public String getDisciplina() {
 		return disciplina;
 	}
