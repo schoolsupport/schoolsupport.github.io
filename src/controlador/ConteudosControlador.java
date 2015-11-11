0c37841be0c4a199e8f3e99f5031b2cbd6fbbdd4
viniciuscrizel <vinicius.crizel845@gmail.com>
2015-11-11 14:27:36 -0200

    ok

--- src/controlador/ConteudosControlador.java
+++ src/controlador/ConteudosControlador.java
@@ -19,6 +19,8 @@ public class ConteudosControlador implements TemplateViewRoute{
 		HashMap dados = new HashMap();
 		dados.put("conteudo", materia);
 		req.session().attribute("conteudo", materia);
+		
+		
 		return new ModelAndView(dados, "conteudos.html");
 	}
 }
