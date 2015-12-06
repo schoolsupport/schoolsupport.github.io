e6082fecadb4cd6640a4def618b1a7b37b825a4c
viniciuscrizel <vinicius.crizel845@gmail.com>
2015-12-05 22:53:36 -0200

    criando parte do esqueci minha senha

--- src/controlador/EnviarSenha.java
+++ src/controlador/EnviarSenha.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -28,22 +31,36 @@ public class EnviarSenha implements TemplateViewRoute{
 			String[] columns = row.split(";");
 			email = columns[2];	
 			senha = columns[3];
+			sendMail(email, senha);
 			//falta so enviar email
 			//criar pagina de erro caso matricula nao exista
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-		res.redirect("index.html");	
-		
-		
-		
-		
-		
-		
-		
-		
-		
+			System.out.println(e.getMessage());
+		}		
 		return new ModelAndView("","index.html");
+	}
+	public void sendMail(String mail, String senha){
+		SimpleEmail email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSslSmtpPort("465");
+		email.setStartTLSRequired(false);
+		email.setSSLOnConnect(false);
+		email.setSSL(true);  
+        email.setTLS(true);  
+		email.setAuthentication("ss.duvidas.pcd@gmail.com",  "informatica123");
+		try {
+		    email.setFrom("ss.duvidas.pcd@gmail.com");
+		    email.setDebug(true); 
+		    email.setSubject("Recuperação de senha");
+		    email.setMsg("Sua senha eh: " + senha);
+		    email.addTo(mail);
+		    
+		    email.send();
+		} catch (EmailException e) {
+		    e.printStackTrace();
+		} 
+		
 	}
 
 }
